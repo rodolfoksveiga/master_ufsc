60e77a2b24c42dc6f5878cd0db77721594509f78
rodolfoksveiga <rodolfoksveiga@gmail.com>
2020-05-12 13:13:57 -0300

    parallelize split_storey.r

--- code/split_storey.r
+++ code/split_storey.r
@@ -1,74 +1,64 @@
+# load libraries ####
+pkgs = c('data.table', 'parallel', 'stringr')
+lapply(pkgs, library, character.only = T)
+
+# base functions ####
+# load data
+LoadData = function(x) as.data.frame(fread(x))
+# remove columns in the data frame related to zones that must be ignored
+RmIgnZone = function(df, ign_zones) {
+  ign_zones = toupper(str_flatten(ign_zones, collapse = '|'))
+  df[, grepl(toupper(ign_zones), colnames(df))] = NULL
+  return(df)
+}
+# write files for each zone
+WriteZoneFile = function(zone, df, file_name, output_dir) {
+  # select the columns related to each interested zone
+  col_names = c('Date/Time', 'Environment:Site Outdoor Air Drybulb',
+                paste0('_', toupper(zone)))
+  col_names = str_flatten(col_names, collapse = '|')
+  df_zone = df[, grepl(col_names, colnames(df))]
+  # write csv file
+  output_path = paste0(output_dir, file_name, '_', zone, '.csv')
+  write.csv(df_zone, output_path)
+  # print '.csv' file name
+  print(output_path)
+}
+# apply the function WriteZoneFile for all the interested zones
+ApplyWZF = function(df, file_name, output_dir, zones) {
+  lapply(zones, WriteZoneFile, df, file_name, output_dir)
+}
+
 # main function ####
-# split_storey()
 # splits the full floor simulation's outputs in individual dwelings
-split_storey = function(input_dir, pattern = NULL, zone_names, ignored_zones = NULL, output_dir) {
-  # input_dir - directory where the full floor simulation's outputs are located
-  # zone_names - names of the zones to be splitted (by default it pick up all the zones in the
+SplitStorey = function(input_dir, output_dir, zones,
+                       pattern = NULL, ign_zones = NULL) {
+  # input_dir: directory where the full floor simulation's outputs are located
+  # zone_names: names of the zones to be splitted (by default it pick up all the zones in the
     # standard model)
-  # ignored_zones - names of zones to be ignored (by default it ignores the 'core', 'cor' (corridor)
-    # and 'bh' (bathroom))
-  # output_dir - directory where the splitted files will be saved
+  # ign_zones: names of zones to be ignored (by default it ignores the 'core', 'cor' (corridor) and
+    # 'bh' (bathroom))
+  # output_dir: directory where the splitted files will be saved
   
-  # name '.csv' files
-  csv_names = dir(input_dir, paste0('*', pattern, '.*csv'))
-  # load, process, separet dwelings in zones and write the .csv files
-  for (i in 1:length(csv_names)) {
-    # count the splitting process
-    print(paste('i =', i))
-    # load files
-    df = as.data.frame(data.table::fread(paste0(input_dir, csv_names[i])))
-    # rename the list which contains the '.csv' files (remove '.csv' suffix)
-    csv_names[i] = stringr::str_remove(csv_names[i], '.csv')
-    # remove columns related to ignored zones
-    if (!is.null(ignored_zones)) {
-      for (ign in ignored_zones) {
-        df[, grepl(toupper(ign), colnames(df))] = NULL
-      }
-    }
-    # split the floor in zones
-    for (zone in zone_names) {
-      # select the columns related to each interested zone
-      df_zone = df[, grepl('Date/Time', colnames(df)) |
-                     grepl('Environment:Site Outdoor Air Drybulb', colnames(df)) |
-                     grepl(paste0('_', toupper(zone)), colnames(df))]
-      # save '.csv' file for each zone
-      file_path = paste0(output_dir, csv_names[i], '_', zone, '.csv')
-      write.csv(df_zone, file_path)
-      # print '.csv' file name
-      print(file_path)
-    }
+  # number of available cores
+  n_cores = detectCores()
+  # load data
+  files_paths = dir(input_dir, paste0('*', pattern, '.*csv'), full.names = T)
+  dfs_list = mclapply(files_paths, LoadData, mc.cores = n_cores)
+  # remove columns related to ignored zones
+  if (!is.null(ign_zones)) {
+    df = mclapply(dfs_list, RmIgnZone, ign_zones, mc.cores = n_cores)
   }
+  # write files
+  files_names = str_remove(basename(files_paths), '.csv')
+  mcmapply(ApplyWZF, dfs_list, files_names, mc.cores = n_cores,
+           MoreArgs = list(output_dir, zones))
 }
-  
+
 # application ####
-simps = c('00', '01')
-wraps = c('c10', 'tv', 'sf')
-storeys = c('floor', 'inter', 'roof')
-conds = c('afn', 'hvac')
-for (simp in simps) {
-  m = 1
-  for (wrap in wraps) {
-    n = 1
-    for (storey in storeys) {
-      o = 1
-      for (cond in conds) {
-        print(paste(simp, '/', toupper(wrap), '/', toupper(storey), '/', toupper(cond)))
-        split_storey(input_dir = c(paste0('/media/rodox/HD_EXTERNO/00.hive/00.hyp/', simp,
-                                          '/0', m, '.', wrap, '/')),
-                     pattern = paste0('hyp_', wrap, '_v', simp, '_', storey, '_', cond),
-                     zone_names = c('sw_dorm_1', 'sw_liv', 'sw_dorm_2', 'se_dorm_2', 'se_liv',
-                                    'se_dorm_1', 'e_dorm_s', 'e_liv', 'e_dorm_n', 'ne_dorm_1',
-                                    'ne_liv', 'ne_dorm_2', 'nw_dorm_2', 'nw_liv', 'nw_dorm_1',
-                                    'w_dorm_n', 'w_liv', 'w_dorm_s'),
-                     ignored_zones = NULL,
-                     output_dir = c(paste0('/media/rodox/HD_EXTERNO/00.hive/00.hyp/', simp,
-                                           '/0', m, '.', wrap, '/0', n, '.', storey, '/0',
-                                           o, '.', cond, '/')))
-        gc()
-        o = o + 1
-      }
-      n = n + 1
-    }
-    m = m + 1
-  }
-}
+SplitStorey('~/going_on/new/', '~/going_on/new/split/',
+            zones = c('sw_dorm_1', 'sw_liv', 'sw_dorm_2', 'se_dorm_2',
+                      'se_liv', 'se_dorm_1', 'e_dorm_s', 'e_liv',
+                      'e_dorm_n', 'ne_dorm_1', 'ne_liv', 'ne_dorm_2',
+                      'nw_dorm_2', 'nw_liv', 'nw_dorm_1', 'w_dorm_n',
+                      'w_liv', 'w_dorm_s'))
