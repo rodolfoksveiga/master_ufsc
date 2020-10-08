36826b952e33b5a65dd49edebbc463eead74b6aa
rodolfoksveiga <rodolfoksveiga@gmail.com>
2020-10-08 14:47:59 -0300

    ready to run main1.r

--- code/main1.r
+++ code/main1.r
@@ -17,14 +17,14 @@ invisible({
   
   # variables ####
   seeds_dir = './seed/'
-  large_models_dir = '~/rolante/master/model/'
+  models_dir = '~/rolante/master/model/'
   shrink_dir = '~/rolante/master/shrink/'
   epws_dir = '~/rolante/weather/'
   weathers = c('curitiba', 'rio_de_janeiro', 'sao_paulo', 'sorriso', 'teresina')
   output_dir = '~/rolante/master/output/'
   split_dir = '~/rolante/master/split/'
   result_dir = '~/rolante/master/result/'
-  cores_left = 6
+  cores_left = 0
   
   # functions ####
   ManageFiles = function(temp_dir, pattern) {
@@ -44,23 +44,24 @@ invisible({
   # build cases
   outputs = c('mean_temp', 'op_temp', 'air_change', 'therm_bal', 'surf_temp')
   mcmapply(BuildModel,
-           seed_path = sample$seed_path,
-           area = NULL, ratio = NULL, height = NULL, azimuth = 0, nstrs = 5,
+           seed_path = sample$seed_path, nstrs = 5,
+           area = NA, ratio = NA, height = NA, azimuth = 0,
            shell_wall = sample$shell_wall, abs_wall = 0.5,
-           abs_roof = 0.6, shell_roof = sample$shell_roof,
-           wwr_liv = NULL, wwr_dorm = NULL, u_window = 5.7, shgc = 0.87, open_factor = 0.45,
-           blind = FALSE, balcony = 0,
+           shell_roof = sample$shell_roof, abs_roof = 0.6,
+           wwr_liv = NA, wwr_dorm = NA, u_window = 5.7, shgc = 0.87, open_factor = 0.45,
+           blind = FALSE, balcony = 0, mirror = FALSE,
+           scale = FALSE, boundary = sample$boundary,
            model_path = sample$model_path,
-           boundary = sample$boundary, scale = FALSE,
            MoreArgs = list(outputs, construction, fill, setup, geometry),
            mc.cores = detectCores() - cores_left)
   # shrink building
-  ApplyShrinkBuild(large_models_dir, 'linear', 5, shrink_dir, cores_left)
+  ApplyShrinkBuild(models_dir, 'linear', 5, shrink_dir, cores_left)
   # run simulations
-  sim_cores = 2
-  ProcessEPSims(NULL, models_dir, epws_dir, weathers, output_dir, sim_cores, inmet)
+  sim_cores_left = detectCores() - 2
+  ProcessEPSims(NULL, models_dir, epws_dir, weathers, output_dir, sim_cores_left, inmet)
+  # ProcessEPSims(NULL, shrink_dir, epws_dir, weathers, split_dir, cores_left, inmet)
   # split outputs
   ApplySplOut(output_dir, 'linear', 5, split_dir, cores_left)
-  # # process outputs
-  # ApplyProcessOut(split_dir, 'linear', 5, result_dir)
+  # process outputs
+  ApplyProcessOut(split_dir, 'linear', 5, result_dir)
 })
